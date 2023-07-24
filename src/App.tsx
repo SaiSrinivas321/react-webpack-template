844b7444670f4327deee8b621f5990a8e45a7f96
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-07-24 17:40:17 +0530

    added React refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { Counter } from "./Counter";
 import "./styles.css";
 export const App = () => {
-  return <h1>React Typescript -{process.env.name}</h1>;
+  return (
+    <>
+      <h1>React Typescript -{process.env.name}</h1>
+      <Counter />
+    </>
+  );
 };
