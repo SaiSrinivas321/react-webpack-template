eab8e4dc0f3e1c2db7b13bb8aed779f0947d9857
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-07-24 16:55:11 +0530

    css and assets loader

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -17,13 +17,25 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
     ],
   },
   output: {
     path: path.resolve(__dirname, "..", "./build"),
     filename: "bundle.js",
   },
-  mode : 'development',
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
